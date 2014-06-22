32e52c90c140c16ad55e0b994b7ab67530df2453
soyuka <soyuka@gmail.com>
2014-06-22 22:51:34 +0200

    Improved daemon

--- cli/commands/daemon.js
+++ cli/commands/daemon.js
@@ -1,15 +1,37 @@
 var spawner = require('../helpers/spawner')
   , helper = require('../helpers/promise')
+  , db = require('ezseed-database').db
 
 module.exports = {
   client: function(client) {
     return function(opts) {
       helper.checkroot()
-			return spawner.spawn(require('ezseed-'+client)('daemon') + ' ' + [opts.command, opts.user].join(' '))
+
+      if(opts.user) {
+			  return
+          spawner.spawn(require('ezseed-'+client)('daemon') + ' ' + [opts.command, opts.user].join(' '))
+          .then(helper.exit(i18n.__('Daemon %s %s', opts.command, client)))
+          .catch(helper.exit(i18n.__('Daemon %s %s', opts.command, client)))
+      } else {
+        db.users.get(function(err, docs) {
+          if(err) throw err
+
+          var num = docs.length, users = []
+
+          while(num--) {
+            users.push(require('ezseed-'+client)('daemon') + ' ' + [opts.command, docs[num].username].join(' '))
+          }
+
+          return spawner.spawn(users.join(' && '))
+          .then(helper.exit(i18n.__('Daemon %s %s', opts.command, client)))
+          .catch(helper.exit(i18n.__('Daemon %s %s', opts.command, client)))
+
+        })
+      }
     }
   },
   ezseed: function(command) {
-    // body...
+
   }
 
 }
