eea308e5697d0498eb2bb468c1b1e53186d099c7
soyuka <soyuka@gmail.com>
2014-07-16 19:42:02 +0200

    (install) Notify to run useradd at the end

--- cli/install.js
+++ cli/install.js
@@ -16,10 +16,10 @@ var p = require('path')
   , helper = require('./helpers/promise')
 
 module.exports = function(opts) {
-	if(opts.new) {
-		require('./helpers/ascii').print()
-		console.log('Welcome to ezseed!')
-	}
+  if(opts.new) {
+    require('./helpers/ascii').print()
+    console.log('Welcome to ezseed!')
+  }
 
   //ask configuration questions
   require('./inquirer/lang')()
@@ -30,58 +30,63 @@ module.exports = function(opts) {
     return helper.condition(!opts.skipconfig, require('./inquirer/config'))
 
   })
-	.then(function(answers) {
+  .then(function(answers) {
 
-		var server
+    var server
 
-		if(answers) {
-			for(var answer in answers) {
-				config[answer] = answers[answer]
-			}
+    if(answers) {
+      for(var answer in answers) {
+        config[answer] = answers[answer]
+      }
 
-			server = '127.0.0.1:'+answers.port
-		}
+      server = '127.0.0.1:'+answers.port
+    }
 
-		return helper.condition(!opts.skipserver, require('./commands/install').server(server))
-	})
-	.catch(helper.exit(i18n.__('Server installation')))
+    return helper.condition(!opts.skipserver, require('./commands/install').server(server))
+  })
+  .catch(helper.exit(i18n.__('Server installation')))
 
   //ask client questions
-	.then(function() {
-		return helper.condition(!opts.skipclient, require('./inquirer/client'))
-	})
-	.then(function(client) {
-		//if opts.skipclient
-		if(client === undefined || opts.skipclient)
-			client = {install_client: false}
+  .then(function() {
+    return helper.condition(!opts.skipclient, require('./inquirer/client'))
+  })
+  .then(function(client) {
+    //if opts.skipclient
+    if(client === undefined || opts.skipclient)
+      client = {install_client: false}
 
-		return helper.condition(client.install_client, require('./commands/install').client(client.client))
-	})
-	.catch(helper.exit(i18n.__('Client installation')))
+    return helper.condition(client.install_client, require('./commands/install').client(client.client))
+  })
+  .catch(helper.exit(i18n.__('Client installation')))
 
   //Write configuration
-	.then(function() {
+  .then(function() {
 
-		if(!opts.skipconfig) {
+    if(!opts.skipconfig) {
 
-			if(fs.existsSync(config_path)) {
-				if(!opts.force) {
-					throw new Error(i18n.__('%s already exists - use force to replace, skipping', config_path))
-				} else {
-					logger.warn(i18n.__('Overriding %s', config_path))
-				}
-			}
+      if(fs.existsSync(config_path)) {
+        if(!opts.force) {
+          throw new Error(i18n.__('%s already exists - use force to replace, skipping', config_path))
+        } else {
+          logger.warn(i18n.__('Overriding %s', config_path))
+        }
+      }
 
-			fs.writeFileSync(config_path, 'module.exports = '+JSON.stringify(config, undefined, 2), {mode: 664})
-			logger.info(i18n.__('Configuration saved in %s', config_path))
+      fs.writeFileSync(config_path, 'module.exports = '+JSON.stringify(config, undefined, 2), {mode: 664})
+      logger.info(i18n.__('Configuration saved in %s', config_path))
 
-		}
+    }
 
-		return helper.next()
-	})
-	.then(function() {
-		return helper.exit('Installation')(0)
-	})
-  .catch(helper.exit('Configuration'))
+    return helper.next()
+  })
+  .then(function() {
+     if(opts.new) {
+      logger.info(i18n.__('Please run "ezseed useradd username"'))
+      return helper.exit('Installation')(0)
+    } else {
+      return helper.exit('Configuration')(0)
+    }
+  })
+  .catch(helper.exit(opts.new ? 'Installation' : 'Configuration'))
 
 }
