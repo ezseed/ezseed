530d78964d0762cc796105911f582bfcc42fe7d3
soyuka <soyuka@gmail.com>
2014-09-18 22:22:04 +0200

    feat(watcher): Added watcher socket configuration

--- watcher.js
+++ watcher.js
@@ -1,3 +1,8 @@
 var config = require('./lib/config')
 
-require('ezseed-watcher')(config)
+config.socket = config.watcher
+config.rpc_socket = config.watcher_rpc
+
+require('ezseed-watcher')(config, function(err, watcher) {
+  if(err) throw err
+})
