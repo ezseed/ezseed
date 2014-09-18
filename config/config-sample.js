c83e4dcb393882c5541e227551417d8c54e2fdaf
soyuka <soyuka@gmail.com>
2014-09-18 22:20:03 +0200

    feat(config): Added watcher socket paths

--- config/config-sample.js
+++ config/config-sample.js
@@ -2,6 +2,8 @@ module.exports =
 {
 	home: '/home', // the path to users home directory (absolute!)
 	tmp: process.env.HOME + '/.ezseed/tmp', //the tmp folder path - default to HOME/.ezseed/tmp (absolute!)
+        watcher: 'unix:///usr/local/opt/ezseed/watcher.sock',
+        watcher_rpc: 'unix:///usr/local/opt/ezseed/watcher_rpc.sock',
 	transmission: false, //is transmission installed?
 	rtorrent: false, //is rtorrent installed?
 	client_link: 'embed', //embed|link do we embed the p2p web interface or should it be openin a new window?
