8df02dfb959adf076fbfdc15346dc624d7784efc
soyuka <soyuka@gmail.com>
2014-09-18 22:21:08 +0200

    refactor(server): Added watcher connexion, logging, theme configuration

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var app = require('express')()
+  , config = require('./lib/config')
+  , logger = require('ezseed-logger')('ezseed')
+  , debug = require('debug')('ezseed:server')
 
 app.use(require('body-parser').json())
+//logger
+app.use(require('morgan')('dev'))
 
-require('ezseed-web')(app)
+require(config.theme)(app)
 require('./api')(app)
 
 
@@ -20,7 +25,53 @@ require('./api')(app)
 // 	setTimeout(function() { process.exit(0) })
 // })
 
+var port = config.port || 8970
 
-app.listen(require('./lib/config').port || 8970)
+require('ezseed-database')(function() {
+  app.listen(port, function() {
+    require('ezseed-logger')('server').log('Listening on ' + port)
+  })
+})
+
+var axon = require('axon')
+  , sock = axon.socket('pull')
+  , socket = config.watcher
+  , rpc_socket = config.watcher_rpc
+  , fs = require('fs')
+  , rpc = require('pm2-axon-rpc')
+  , req = axon.socket('req')
+  , rep = axon.socket('rep')
+
+process.watcher = {}
+process.watcher.client = new rpc.Client(req)
+
+try {
+    debug('Watcher socket: %s', socket)
+
+    sock.connect(socket)
+
+    sock.on('connect', function() {
+      //create a path to watch
+      logger.log('Watcher sock %s connected', socket)
+
+      req.connect(rpc_socket)
+
+      // client.call('refresh', paths, function(err, n){
+      //     console.log(n);
+      // })
+
+    })
+
+    sock.on('message', function(msg, update){
+      //notify client for update
+    })
+} catch(e) {
+  logger.error('Watcher not found on port %s', socket)
+  logger.error(e)
+}
+
+process.on('unhandledException', function(e) {
+  console.log(e) 
+})
 
 module.exports = app
