db91834fc3a1cc9bcdb01970a9504b3d9d11b88b
soyuka <soyuka@gmail.com>
2014-09-28 20:45:41 +0200

    fix(useradd): added some i18n

--- cli/useradd.js
+++ cli/useradd.js
@@ -57,15 +57,15 @@ module.exports = function(opts) {
       //adds the system user
       return require('./commands/user')
       .create(opts.username, opts.password)
-      .catch(helper.exit('Creating system user.'))
+      .catch(helper.exit(i18n.__('Creating system user')))
       .then(function() {
 
           //adds the client user (transmission or rtorrent)
         return require('./commands/user')
           .client(opts.client)('useradd', opts.username, opts.password, opts.transmission_port)
       })
-      .then(helper.exit('Creating %s', opts.username))
-      .catch(helper.exit('Creating %s client', opts.client))
+      .then(helper.exit(i18n.__('Creating %s', opts.username)))
+      .catch(helper.exit(i18n.__('Creating %s client', opts.client)))
 
     })
 
