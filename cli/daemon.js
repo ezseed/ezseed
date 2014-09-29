5996565197f83777f2d42d3e73f0cbc3444ac83a
soyuka <soyuka@gmail.com>
2014-09-29 21:34:32 +0200

    fix(daemon): client typo

--- cli/daemon.js
+++ cli/daemon.js
@@ -1,6 +1,6 @@
 module.exports = function(client) {
   return function(opts) {
-    require('./commands/daemon')(client)(opts)
+    require('./commands/daemon').client(client)(opts)
     .then(helper.exit(i18n.__('Daemon %s %s', opts.command, client)))
     .catch(helper.exit(i18n.__('Daemon %s %s', opts.command, client)))
   }
