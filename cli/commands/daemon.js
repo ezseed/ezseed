566063e26f6d2423874a492a99f7285db1b9aad2
soyuka <soyuka@gmail.com>
2014-10-03 08:28:27 +0200

    fix(daemon): pm2 does not init itself with connect

--- cli/commands/daemon.js
+++ cli/commands/daemon.js
@@ -3,6 +3,8 @@ var spawner = require('../helpers/spawner')
   , logger = require('ezseed-logger')('daemon')
   , db = require('ezseed-database').db
   , p = require('path')
+  , fs = require('fs')
+  , exec = require('child_process').exec
 
 module.exports = {
   client: function(client) {
@@ -43,60 +45,70 @@ module.exports = {
     return function(options) {
 
       var pm2 = require('pm2')
+
       var json = p.join(require('../../constants').HOME, 'ezseed.json')
       var exit = 'Ezseed '+command
 
-      pm2.connect(function(err) {
-        if(err) {
-          logger.error('Error while connecting with pm2', err)
-          return helper.exit(exit)(1)
-        }
+      //pm2 does not init itself on connect
+      var pm2_bin = p.resolve(__dirname, '../../', 'node_modules', 'pm2', 'bin', 'pm2')
 
-        switch(command) {
-          case 'start':
-            pm2.startJson(json, {}, null, function(err) {
-              if(err) {
-                logger.error('Error while starting process', err)
-              }
+      if(!fs.existsSync(pm2_bin)) {
+        pm2_bin = 'pm2'
+      }
 
-              return helper.exit(exit)(err ? 1 : 0)
-            })
-            break;
-          case 'stop':
-            pm2.stop('ezseed', function(err) {
-              if(err) {
-                logger.error('Error while stopping ezseed', err)
-              }
-
-              pm2.stop('watcher', function(err) {
-                if(err) {
-                  logger.error('Error while stopping watcher', err)
-                }
-
-                return helper.exit(exit)(err ? 1 : 0)
-              })
-            })
-            break;
-          case 'restart':
-            pm2.restart('ezseed', function(err) {
-              if(err) {
-                logger.error('Error while restarting ezseed', err)
-              }
-
-              pm2.restart('watcher', function(err) {
-                if(err) {
-                  logger.error('Error while restarting watcher', err)
-                }
-
-                return helper.exit(exit)(err ? 1 : 0)
-              })
-            })
-            break;
-          default:
-            logger.warn(i18n.__('No such command %s', command))
+      exec(pm2_bin, ['-v'], function() {
+        pm2.connect(function(err) {
+          if(err) {
+            logger.error('Error while connecting with pm2', err)
             return helper.exit(exit)(1)
-        }
+          }
 
+          switch(command) {
+            case 'start':
+              pm2.startJson(json, {}, null, function(err) {
+                if(err) {
+                  logger.error('Error while starting process', err)
+                }
+
+                return helper.exit(exit)(err ? 1 : 0)
+              })
+              break;
+            case 'stop':
+              pm2.stop('ezseed', function(err) {
+                if(err) {
+                  logger.error('Error while stopping ezseed', err)
+                }
+
+                pm2.stop('watcher', function(err) {
+                  if(err) {
+                    logger.error('Error while stopping watcher', err)
+                  }
+
+                  return helper.exit(exit)(err ? 1 : 0)
+                })
+              })
+              break;
+            case 'restart':
+              pm2.restart('ezseed', function(err) {
+                if(err) {
+                  logger.error('Error while restarting ezseed', err)
+                }
+
+                pm2.restart('watcher', function(err) {
+                  if(err) {
+                    logger.error('Error while restarting watcher', err)
+                  }
+
+                  return helper.exit(exit)(err ? 1 : 0)
+                })
+              })
+              break;
+            default:
+              logger.warn(i18n.__('No such command %s', command))
+              return helper.exit(exit)(1)
+          }
+
+        })
       })
     }
      
