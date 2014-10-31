c26514d44682fdf58d84e5bdb50ba039dd838433
soyuka <soyuka@gmail.com>
2014-10-31 19:32:04 +0100

    refactor(useradd): Removed async waterfall, use promise Add the transmission port before anything else so that it would be added to the user creation (saves a few code lines)

--- cli/useradd.js
+++ cli/useradd.js
@@ -10,93 +10,91 @@ var helper = require('./helpers/promise')
 
 module.exports = function(opts) {
 
-  require('./inquirer/useradd.js')(opts)
-  .then(function(opts) {
-    //require config + client installed?
+  require('./inquirer/useradd.js')()
+  //find transmission port
+  .then(function() {
 
-    async.waterfall([
-      function(done) {
-        return db.user.create(opts, function(err) {
-          if(err) {
-            logger.error(err)
-          }
+    return new Promise(function(resolve, reject) {
+      if(opts.client !== 'transmission') {
+        return resolve(opts)
+      }
 
-          return done() //forcing no arguments to prevent async.waterfall to add those
-        })
-      },
-      function(done) {
-        //adds the ezseed user
-        db.paths.save(
-          p.join(config.home, opts.username, 'downloads'),
-          {
-            username: opts.username,
-            default: true
-          },
-          function(err) {
-            if(err) {
-              logger.warn(err)
-            }
-
-            return done()
-          }
-        )
-      }, function(done) {
-
-        if(opts.client !== 'transmission') {
-          return done()
+      //searching for an open port for the transmission configuration
+      require('local-port').findOpen(9002, function(err, open_port) {
+        if(err) {
+          logger.error('We could not find any open port')
+          return reject(err)
+        } else {
+          debug('Transmission port', open_port)
+          opts.port = open_port
+          return resolve(opts)
         }
-
-        require('local-port').findOpen(9002, function(err, open_port) {
-          if(err) {
-            logger.error('We could not find any open port')
-            done(err)
-          } else {
-            opts.transmission_port = open_port
-            done()
-          }
-        })
-      }
-    ], function(err) {
-      if(err) {
-        helper.exit(err)(1)
-      }
-
-      //adds the system user
-      return require('./commands/user')
-      .create(opts.username, opts.password)
-      .catch(helper.exit(i18n.__('Creating system user')))
-      .then(function() {
-
-        debug('Created system user')
-        //adds the client user (transmission or rtorrent)
-        return require('./commands/user')
-          .client(opts.client)('useradd', opts.username, opts.password, opts.transmission_port)
       })
-      .catch(helper.exit(i18n.__('Creating %s client', opts.client)))
-      .then(function() {
-        debug('Created client')
-        return helper.condition(opts.client == 'transmission', function() {
-          return new Promise(function(resolve, reject) {
-            db.user.update(opts.username, {port: opts.transmission_port}, function(err) {
-              if(err) {
-                return reject(err)
-              } else {
-                return resolve(0)
-              }
-            })
-          })
-        }, function() {
-          return helper.next(0)
-        })
-      })
-      .then(function() {
-        return require('./commands/daemon').client(opts.client)({command: 'start', user: opts.username})
-      })
-      .then(helper.exit(i18n.__('Creating %s', opts.username)))
-      .catch(helper.exit(i18n.__('Creating %s', opts.username)))
 
     })
-
-
   })
+  .catch(helper.exit(i18n.__('Creating system user')))
+  .then(function() {
+    //@todo require config + client installed?
+    //create user in database
+    return new Promise(function(resolve, reject) {
+
+      db.user.create(opts, function(err) {
+        if(err) {
+          return reject(err, opts)
+        }
+
+        debug('db user created', opts)
+
+        return resolve(opts)
+
+      })
+
+    })
+  })
+  .catch(helper.exit(i18n.__('Creating %s', opts.username)))
+  //save the download path
+  .then(function() {
+
+    return new Promise(function(resolve, reject) {
+
+     //adds the download path to the user
+     db.paths.save(
+       p.join(config.home, opts.username, 'downloads'),
+       {
+         username: opts.username,
+         default: true
+       },
+       function(err) {
+         //this can be a user mistake, path is already watched
+         if(err) {
+           logger.warn(err)
+         }
+
+         debug('Path saved', opts)
+
+         return resolve(opts)
+
+      })
+
+    })
+  })
+  //add system user
+  .then(function() {
+    return require('./commands/user')
+      .create(opts.username, opts.password)
+  })
+  .catch(helper.exit(i18n.__('Creating system user')))
+   //adds the client user (transmission or rtorrent)
+  .then(function() {
+    return require('./commands/user')
+      .client(opts.client)('useradd', opts.username, opts.password, opts.port)
+  })
+  .catch(helper.exit(i18n.__('Creating %s client', opts.client)))
+  .then(function() {
+    return require('./commands/daemon').client(opts.client)({command: 'start', user: opts.username})
+  })
+  .then(helper.exit(i18n.__('Creating %s', opts.username)))
+  .catch(helper.exit(i18n.__('Creating %s', opts.username)))
+
 }
